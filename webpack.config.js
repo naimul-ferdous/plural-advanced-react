789aca3b1c6a513d90d444e4f177c25d5e47e9dc
Naimul Ferdous <ferdousnaimul@gmail.com>
2020-05-06 14:29:14 +0600

    done Subscribing to an External State

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,13 @@
-const webpack = require('webpack');
 const path = require('path');
 
 const config = {
   resolve: {
     modules: [
       path.resolve('./lib'),
-      path.resolve('./node_modules')
+      path.resolve('./node_modules'),
     ]
   },
-  entry: [
-    'babel-polyfill',
-    'react-hot-loader/patch',
-    './lib/renderers/dom.js'
-  ],
+  entry: ['babel-polyfill', './lib/renderers/dom.js'],
   output: {
     path: path.resolve(__dirname, 'public'),
     filename: 'bundle.js'
@@ -20,56 +15,22 @@ const config = {
   module: {
     rules: [
       {
-        test: /\.(js|jsx)$/,
-        use: 'babel-loader',
-        exclude: /node_modules/
-      },
-      {
-        test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader'
-        ],
-        exclude: /\.module\.css$/
-      },
-      {
-        test: /\.css$/,
-        use: [
-          'style-loader',
-          {
-            loader: 'css-loader',
-            options: {
-              importLoaders: 1,
-              modules: true
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+
+        options: {
+          presets: [
+            '@babel/preset-env',
+            {
+              plugins: [
+                '@babel/plugin-proposal-class-properties'
+              ]
             }
-          }
-        ],
-        include: /\.module\.css$/
-      },
-      {
-        test: /\.png$/,
-        use: [
-          {
-            loader: 'url-loader',
-            options: {
-              mimetype: 'image/png'
-            }
-          }
-        ]
+          ]
+        },
       }
     ]
-  },
-  resolve: {
-    extensions: [
-      '.js',
-      '.jsx'
-    ],
-    alias: {
-      'react-dom': '@hot-loader/react-dom'
-    }
-  },
-  devServer: {
-    contentBase: './public'
   }
 };
 
